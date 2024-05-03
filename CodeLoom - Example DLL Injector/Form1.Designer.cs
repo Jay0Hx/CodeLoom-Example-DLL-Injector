8c60560158654a5487d4bf354e4500bf2560bc13
james <james@JAYS_PC>
2024-05-03 08:49:31 +0100

    Add project files.

---
+++ CodeLoom - Example DLL Injector/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CodeLoom___Example_DLL_Injector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processList = new System.Windows.Forms.ComboBox();
+            this.injectButton = new System.Windows.Forms.Button();
+            this.dllPathTEST = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // processList
+            // 
+            this.processList.FormattingEnabled = true;
+            this.processList.Location = new System.Drawing.Point(32, 224);
+            this.processList.Name = "processList";
+            this.processList.Size = new System.Drawing.Size(294, 21);
+            this.processList.Sorted = true;
+            this.processList.TabIndex = 0;
+            // 
+            // injectButton
+            // 
+            this.injectButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.injectButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.injectButton.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.injectButton.Location = new System.Drawing.Point(251, 251);
+            this.injectButton.Name = "injectButton";
+            this.injectButton.Size = new System.Drawing.Size(75, 23);
+            this.injectButton.TabIndex = 1;
+            this.injectButton.Text = "Inject";
+            this.injectButton.UseVisualStyleBackColor = true;
+            this.injectButton.Click += new System.EventHandler(this.injectButton_Click_1);
+            // 
+            // dllPathTEST
+            // 
+            this.dllPathTEST.Location = new System.Drawing.Point(13, 48);
+            this.dllPathTEST.Name = "dllPathTEST";
+            this.dllPathTEST.Size = new System.Drawing.Size(313, 20);
+            this.dllPathTEST.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 305);
+            this.Controls.Add(this.dllPathTEST);
+            this.Controls.Add(this.injectButton);
+            this.Controls.Add(this.processList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox processList;
+        private System.Windows.Forms.Button injectButton;
+        private System.Windows.Forms.TextBox dllPathTEST;
+    }
+}
+
---
+++ CodeLoom - Example DLL Injector/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CodeLoom___Example_DLL_Injector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadProcesses();
+        }
+
+        private void LoadProcesses()
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                processList.Items.Add(process.ProcessName);
+            }
+        }
+
+        private void injectButton_Click_1(object sender, EventArgs e)
+        {
+            string processName = processList.SelectedItem.ToString();
+            string dllpath = dllPathTEST.Text;
+
+            //Check to make sure that the soft cunt has selected a process && DLL...
+            if (string.IsNullOrEmpty(processName) || string.IsNullOrEmpty(dllpath))
+            {
+                MessageBox.Show("silly cunt");
+                return;
+            }
+
+            //Double check that the process that they have selected is real and not some dud shit.
+            Process[] processes = Process.GetProcessesByName(processName);
+            if (processes.Length == 0)
+            {
+                MessageBox.Show("silly cunt 2");
+                return;
+            }
+
+            foreach (Process process in processes)
+            {
+                IntPtr hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, process.Id);
+                if (hProcess == IntPtr.Zero)
+                {
+                    MessageBox.Show("Silly cunt for the third time.");
+                    return;
+                }
+            }
+
+            foreach (Process process in processes)
+            {
+                IntPtr hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, process.Id);
+                if (hProcess == IntPtr.Zero)
+                {
+                    MessageBox.Show("cant open the cunting process.");
+                    return;
+                }
+
+                IntPtr pDllPath = IntPtr.Zero;
+                IntPtr hThread = IntPtr.Zero;
+                try
+                {
+                    // Allocate memory in the target process for the DLL path
+                    pDllPath = Marshal.StringToHGlobalAnsi(dllpath);
+                    IntPtr pLoadLibrary = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+                    // Constants for error codes (you can define these or use their actual values)
+                    const int ERROR_ACCESS_DENIED = 5;
+                    const int ERROR_FILE_NOT_FOUND = 2;
+
+                    // Create a remote thread in the target process to load the DLL
+                    hThread = CreateRemoteThread(hProcess, IntPtr.Zero, 0, pLoadLibrary, pDllPath, 0, IntPtr.Zero);
+                    if (hThread == IntPtr.Zero)
+                    {
+                        int errorCode = Marshal.GetLastWin32Error();
+                        string errorMessage = $"Failed to create remote thread to load DLL. Error code: {errorCode}";
+
+                        switch (errorCode)
+                        {
+                            case ERROR_ACCESS_DENIED:
+                                errorMessage += "Permissions are too low you sillt cunt";
+                                break;
+                            case ERROR_FILE_NOT_FOUND:
+                                errorMessage += " Process has fucked off.";
+                                break;
+
+                            default:
+                                errorMessage += "Not a fucking scoobys mate";
+                                break;
+                        }
+
+                        MessageBox.Show(errorMessage);
+                        return;
+                    }
+
+
+                    // Wait for the remote thread to finish
+                    WaitForSingleObject(hThread, 0xFFFFFFFF);
+                }
+                finally
+                {
+                    // Cleanup
+                    CloseHandle(hThread);
+                    Marshal.FreeHGlobal(pDllPath);
+                    CloseHandle(hProcess);
+                }
+
+                MessageBox.Show("Shes fuckin in boys.");
+            }
+        }
+
+        //Import shit
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess,
+           IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter,
+           uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll")]
+        static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int CloseHandle(IntPtr hObject);
+
+        const uint PROCESS_ALL_ACCESS = 0x1F0FFF;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
